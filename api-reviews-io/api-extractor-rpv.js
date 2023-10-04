8dbf4c722977d4753c97559a04fa595b4e93c090
Edwin <edwin600@live.com>
2023-10-04 11:42:23 -0600

    api-reviews extractor fixed

--- api-reviews-io/api-extractor-rpv.js
+++ api-reviews-io/api-extractor-rpv.js
@@ -1,14 +1,17 @@
 async (fnParams, page, extractorsDataObj, {_, Errors})=> {
+  // this custom has to be place in perVariant function custom
+  // the store variable is the only one thing you have to change
     let variants = extractorsDataObj.customData.variants
     const skuArray = variants.map(item => item.sku);
     const idArray = variants.map(item => item.id);
     const concatenatedString = [...skuArray, ...idArray].join('%3B');
 
     let items = await page.evaluate(async(concatenatedString)=>{
-        let reviewsTotal = Number(document.querySelector(".header__group .R-TextBody")?.textContent.match(/\d+/)[0]|0)
-        let urlToFetch = `https://api.reviews.io/timeline/data?type=product_review&store=gigi-pip&sort=date_desc&page=1&per_page=${reviewsTotal}&sku=${concatenatedString}&lang=en&enable_avatars=true&include_subrating_breakdown=1`
+      const store = 'beauty-bakerie' //change this store variable
+        let reviewsTotal = Number(document.querySelector("div .ruk-rating-snippet-count")?.textContent.match(/\d+/)?.[0]|0)
+        let urlToFetch = `https://api.reviews.io/timeline/data?type=product_review&store=${store}&sort=date_desc&page=1&per_page=${reviewsTotal}&sku=${concatenatedString}&lang=en&enable_avatars=true&include_subrating_breakdown=1`
         let response = await fetch(urlToFetch)
-        let data = await response.json()
+        let data = await response.json() 
         let items = data.timeline.map(e => {
           return{
               content:e._source.comments,
@@ -33,6 +36,8 @@ async (fnParams, page, extractorsDataObj, {_, Errors})=> {
      },
     "items": items
   }
-  extractorsDataObj.customData.reviews = reviews
+  if (reviews?.items?.length){
+    extractorsDataObj.customData.reviews = reviews
+  }
 
 }
