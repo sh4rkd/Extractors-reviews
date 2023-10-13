14364dd7542c514d6bfbec3805770e3ce9a89f50
Edwin <edwin600@live.com>
2023-10-13 12:31:26 -0600

    changes on opinew reviews custom function

--- opinew-reviews/opinew-rpnc.js
+++ opinew-reviews/opinew-rpnc.js
@@ -1,8 +1,8 @@
-async (fnParams, page, extractorsDataObj, {_, Errors}) => {
+async (fnParams, page, extractorsDataObj, {_, parseUrl, Errors}) => {
     // copy and paste this code in a postNavigateCustom and add a custom strategy for reviews adding the path "reviews"
     const { origin } = new URL(extractorsDataObj.pageData.url) 
     async function GetAllReviews(origin){
-        try{
+        try{ 
             return await page.evaluate(async(origin) => {
                 const fetchHeaders = {
                     headers: {
@@ -42,26 +42,26 @@ async (fnParams, page, extractorsDataObj, {_, Errors}) => {
                 }
                 
 
-                const productId = window.meta.product.id
+                const productId = '4835083812925' || window.meta.product.id
                 const firstRes = await fetch(GetUrlToFetch(productId, 1), fetchHeaders)
                 const firstJson = firstRes.status == 200 ? await firstRes.json() : {}
+                const reviewsLen = firstJson?.reviews?.length
                 const totalOfPages = firstJson.review_page_count || 0
                 const totalPerBatch = 25 //don't increase this number, otherwise we will get 429 error
-                const totalOfRequests = totalOfPages > (totalPerBatch - 1) ? Math.ceil(totalOfPages / totalPerBatch)  : 0
+                const totalOfRequests = totalOfPages > (totalPerBatch - 1) ? Math.ceil(totalOfPages / totalPerBatch)  : 1
                 
                 let init =  0
                 let final = totalPerBatch
-                const averageRating = firstJson?.product?.average_rating || firstJosn?.product?.average_stars || ''
+                const averageRating = firstJson?.product?.average_rating || firstJson?.product?.average_stars || ''
                 const promises = []
                 let reviewsToReturn = []
                 if (totalOfPages > 0){
-
                     for (let j = 0; j < totalOfRequests; j++){
                         final = ((j + 1) == totalOfRequests) ? totalOfPages : totalPerBatch*(j + 1)
                         for (let i = init; i < final; i++){
                             const newUrl = GetUrlToFetch(productId, i + 1)
                             promises.push(fetch(newUrl, fetchHeaders))
-    
+     
                         }
                         init = final
                         const allReviews = await GetAllPromises(promises)
@@ -70,14 +70,13 @@ async (fnParams, page, extractorsDataObj, {_, Errors}) => {
                             reviewsToReturn = [...reviewsToReturn, ...items]
                             return
                         })
-                        await new Promise(r => setTimeout(r, 20000))
+                        await new Promise(r => setTimeout(r, 5000))
                         if (j == 1){
                             // remove this block to test, if you dont get reviews then keep it here.
                             break
                         }
                     }
-                    
-                    return {
+                    const finalObj = {
                         "overallRating": {
                           "value": Number(averageRating)?.toFixed(1),
                           "max": "5",
@@ -85,6 +84,7 @@ async (fnParams, page, extractorsDataObj, {_, Errors}) => {
                         },
                         "items": reviewsToReturn
                     }
+                    return finalObj
                 }
                 return {}
 
