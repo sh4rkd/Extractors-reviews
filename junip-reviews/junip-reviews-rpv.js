c5aa87571b63f863800092b8a5a5a1b67cfbeab8
Edwin <edwin600@live.com>
2023-11-29 13:53:07 -0600

    junip reviews strategy updated

--- junip-reviews/junip-reviews-rpv.js
+++ junip-reviews/junip-reviews-rpv.js
@@ -1,148 +1,117 @@
-;async (fnParams, page, extractorsDataObj, { _, Errors }) => {
-  // add this custom in a perVariant custom
+async (fnParams, page, extractorsDataObj, { _, Errors }) => {
+  // >> activate the disable-web-security option in navigation options 
+  // >> add this custom in a perVariant custom       
   // find the juni store key in newwork tab and change the below variable
   // example url: https://api.juniphq.com/v1/product_reviews?include=product%2Ccustomer%2Cstore%2Crespondent%2Csurvey_answers%2Ctik_tok_urls&filter%5Bproduct_remote_ids%5D%5B%5D=6889983541386&sort%5Bfield%5D=created_at&sort%5Border%5D=desc&page%5Bsize%5D=100&v=TRWyAzKgXFVMsgdtPLyjeUEo
   // this would be the key: TRWyAzKgXFVMsgdtPLyjeUEo (from the example url)
-  const juniStorekey = "TRWyAzKgXFVMsgdtPLyjeUEo"
+  const junipStoreKey = "DDeH8DGMqDUA4ojWZVoazg5L" //this is all you need to change
 
-  const { id } = extractorsDataObj.customData
-  let { url } = extractorsDataObj.pageData
-  let domain = new URL(url).origin
+  const { id: productId } = extractorsDataObj.customData
+  let { origin } = new URL(extractorsDataObj.pageData.url)
+  const fetchHeader = getFetchHeader(origin, junipStoreKey)
+  const {
+    id: reviewsID, 
+    rating_average: averageRating,
+    rating_count: totalReviews
+  } = await fetchReviewDetails(junipStoreKey, productId, fetchHeader)
+  const maxReviewsPerRequest = 100
+
   const config = {
-    id,
-    domain,
-    juniStorekey,
+      junipStoreKey,
+      productId, 
+      origin,
+      fetchHeader,
+      reviewsID,
+      maxReviewsPerRequest,
+      averageRating,
+      totalReviews
   }
-  try {
-    let dataToFetch = await page.evaluate((config) => {
-      let junipStoreKey =
-        document
-          .querySelector(".junip-store-key[data-store-key]")
-          ?.getAttribute("data-store-key") || config.juniStorekey
+  const reviews = await fetchReviews(config)
 
-      let totalReviews =
-        Number(
-          document
-            .querySelector(".junip-product-review-count")
-            ?.textContent?.replace(/\,||\./g, "")
-            .match(/\d+/)[0] | 0
-        ) ||
-        Number(
-          Array.from(document.querySelectorAll("script"))
-            .filter((element) => element.outerHTML.includes("reviewCount"))[0]
-            .outerHTML.match(/(?<=reviewCount":)\d+(?=,)/g)[0]
-        ) ||
-        "" //modificar el selector de total reviews. || Se añadió el path the {{ Number(Array.from(document.querySelectorAll("script")).filter(element => element.outerHTML.includes('reviewCount'))[0].outerHTML.match(/(?<=reviewCount":)\d+(?=,)/g)[0])] }}, en el caso de que la página no esté cargando correctamente y sea necesario sacar el total reviews del script. NOTA: Si se hace esto, es importante validar si se tiene que inyectar el script en una postNavigate
-
-      let reviewId =
-        window?.__st?.rid ||
-        window?.meta?.product?.id ||
-        window?.sswApp?.product?.id ||
-        window?.ShopifyAnalytics?.meta?.page?.resourceId ||
-        id ||
-        "" // Esto es solo para pruebas en una herramienta de scraping
-
-      let quantityReviews = 100
-
-      let referrer = `${config.domain}/`
-
-      return {
-        junipStoreKey,
-        reviewId,
-        totalReviews,
-        quantityReviews,
-        referrer,
-      }
-    }, config)
-
-    let items = await page.evaluate(async (dataToFetch) => {
-      let { junipStoreKey, reviewId, totalReviews, quantityReviews, referrer } =
-        dataToFetch
-      let reviewsToParse = []
-      const junipFetch = async (link, junipStoreKey, referrer) => {
-        let dataFetch = await fetch(link, {
-          headers: {
-            accept: "*/*",
-            "accept-language": "es-ES,es;q=0.9",
-            "content-type": "application/json",
-            "junip-store-key": junipStoreKey,
-            "sec-ch-ua":
-              '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"',
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "cross-site",
-          },
-          referrer: referrer,
-          referrerPolicy: "strict-origin-when-cross-origin",
-          body: null,
-          method: "GET",
-          mode: "cors",
-          credentials: "omit",
-        })
-        return await dataFetch.json()
-      }
-
-      let dataFirstFetch = await junipFetch(
-        `https://api.juniphq.com/v1/product_reviews?include=product%2Ccustomer%2Cstore%2Crespondent%2Csurvey_answers%2Ctik_tok_urls&filter%5Bproduct_remote_ids%5D%5B%5D=${reviewId}&sort%5Bfield%5D=created_at&sort%5Border%5D=desc&page%5Bsize%5D=${quantityReviews}&v=${junipStoreKey}`,
-        junipStoreKey,
-        referrer
-      )
-      reviewsToParse.push(dataFirstFetch.product_reviews)
-
-      if (totalReviews > quantityReviews) {
-        totalReviews -= quantityReviews
-        let lastProductId =
-          dataFirstFetch.product_reviews.slice(-1)[0].product_id
-        let pageAfter = dataFirstFetch.meta.page.after
-
-        while (totalReviews > 0) {
-          let lastReviewFetch = await junipFetch(
-            `https://api.juniphq.com/v1/product_reviews?include=product%2Ccustomer%2Crespondent%2Csurvey_answers%2Ctik_tok_urls&page%5Bsize%5D=${quantityReviews}&filter%5Bproduct_ids%5D%5B%5D=${lastProductId}&sort%5Bfield%5D=created_at&sort%5Border%5D=desc&page%5Bafter%5D=${pageAfter}&v=${junipStoreKey}`,
-            junipStoreKey,
-            referrer
-          )
-          reviewsToParse.push(lastReviewFetch.product_reviews)
-          totalReviews -= lastReviewFetch.product_reviews.length
-          lastProductId =
-            lastReviewFetch.product_reviews.slice(-1)[0].product_id
-          pageAfter = lastReviewFetch.meta.page.after
+  async function fetchReviews(config){
+    try{
+      return await page.evaluate(async(config) => {
+        if (config.totalReviews < 1){
+          return {}
         }
-      }
-      let reviews = [].concat(...reviewsToParse).map((e) => {
-        return {
-          content: e?.body,
-          title: e?.title,
-          date: e?.created_at?.split("T")[0] ?? "",
-          userName: `${e?.customer?.first_name} ${e?.customer?.last_name}`,
-          rating: {
-            value: e?.rating | 0,
+        const firstUrlToFetch = `https://api.juniphq.com/v1/product_reviews?include=product%2Ccustomer%2Cstore%2Crespondent%2Csurvey_answers%2Ctik_tok_urls&filter%5Bproduct_remote_ids%5D%5B%5D=${config.productId}&sort%5Bfield%5D=created_at&sort%5Border%5D=desc&page%5Bsize%5D=${config.maxReviewsPerRequest}&v=${config.junipStoreKey}`
+        const firstRes = await fetch(firstUrlToFetch, config.fetchHeader)
+        const firstJson = firstRes.status == 200 ? await firstRes.json() : {}
+        let afterKey = firstJson?.meta?.page?.after || ''
+        const allReviews = firstJson?.product_reviews || []
+        while(afterKey){
+          const nextUrlToFetch = `https://api.juniphq.com/v1/product_reviews?include=product%2Ccustomer%2Cstore%2Crespondent%2Csurvey_answers%2Ctik_tok_urls&page%5Bsize%5D=${config.maxReviewsPerRequest}&filter%5Bproduct_ids%5D%5B%5D=${config.reviewsID}&sort%5Bfield%5D=created_at&sort%5Border%5D=desc&page%5Bafter%5D=${afterKey}&v=${config.junipStoreKey}`
+          const nextRes = await fetch(nextUrlToFetch, config.fetchHeader)
+          const nextJson = nextRes.status == 200 ? await nextRes.json() : {}
+          nextJson?.product_reviews?.map(e => allReviews.push(e))
+          afterKey = nextJson?.meta?.page?.after
+        }
+        const items = allReviews?.map(e => getReviewObj(e))
+
+        function getReviewObj(review){
+          return {
+            content: review?.body,
+            title: review?.title,
+            date: review?.created_at?.split("T")?.[0] ?? "",
+            userName: `${review?.customer?.first_name || ''} ${review?.customer?.last_name || ''}`,
+            rating: {
+              value: review?.rating || 0,
+              max: "5",  
+              type: "stars",
+            } 
+          }
+        }
+        const reviewsToReturn = {
+          overallRating: {
+            value: isNaN(Number(config.averageRating))
+              ? ""
+              : Number(config.averageRating).toFixed(1),
             max: "5",
             type: "stars",
           },
+          items
         }
-      })
-      return reviews
-    }, dataToFetch)
+        return items?.length ? reviewsToReturn : {}
+      }, config)
+    }catch(e){} 
+  }
 
-    const sumRatingValues = items.reduce(
-      (sum, review) => sum + review.rating.value,
-      0
-    )
-    const averageRating = sumRatingValues / items.length
-    let reviews = {
-      overallRating: {
-        value: isNaN(Number(averageRating))
-          ? ""
-          : Number(averageRating).toFixed(1),
-        max: "5",
-        type: "stars",
-      },
-      items: items,
+  async function fetchReviewDetails(junipStoreKey, productId, fetchHeader){
+    const config = {
+      junipStoreKey, productId, fetchHeader
     }
-    if (reviews?.items?.length) {
-      extractorsDataObj.customData.reviews = reviews
-    }
-  } catch {}
+      try{
+          return await page.evaluate(async(config) => {
+              const urlToFetch = `https://api.juniphq.com/v1/products?filter%5Bremote_ids%5D=${config.productId}&v=${config.junipStoreKey}`
+              const res = await fetch(urlToFetch, config.fetchHeader)
+              const data = res.status == 200 ? await res.json() : {}
+              return data?.products?.[0] || {}
+          }, config)
+      }catch(e){}
+  }   
+  function getFetchHeader(origin, junipStoreKey){
+      return {
+        headers: {
+          accept: "*/*",
+          "accept-language": "es-ES,es;q=0.9",
+          "content-type": "application/json",
+          "junip-store-key": junipStoreKey,
+          "sec-ch-ua":
+            '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"Windows"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "cross-site",
+        },
+        origin,
+        referrer: `${origin}/`,
+        referrerPolicy: "strict-origin-when-cross-origin",
+        body: null,
+        method: "GET",
+        mode: "cors",
+        credentials: "omit",
+      }
+  }
+  extractorsDataObj.customData.reviews = reviews
 }
