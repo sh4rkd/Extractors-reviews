4d938490527fa1ba4222bc92b84aed7707477311
Edwin <edwin600@live.com>
2023-12-07 13:11:39 -0600

    shopify clicker modified

--- shopify-options-clicker/shopify-clicker.js
+++ shopify-options-clicker/shopify-clicker.js
@@ -1,12 +1,14 @@
 async (fnParams, page, extractorsDataObj, {_, Errors})=> {
     /**
      * this custom has to be added in a perVariant custom function
-     * you only have to change or add selectors and specify if you want it to match with textContent or with attribute
      */
     
-     
+     /**
+      * elementsToMatch and elementsToClick MUST select the same amount of elements
+      */
     const config = {
-        optionsSelector: 'div [role="radiogroup"] label[for*="option"] + div button',//add selectors to get all PDP options
+        elementsToMatch: 'div [role="radiogroup"] [for*="option"] input',
+        elementsToClick: 'div [role="radiogroup"] [for*="option"] input + label',
         matchWith: 'attribute', // | textContent
         timeout: 3000 //time(in ms) of every click
     }
@@ -15,25 +17,47 @@ async (fnParams, page, extractorsDataObj, {_, Errors})=> {
  
     async function clickVariantOptions(config){
         try{
-            const variantOptions = extractorsDataObj.customData.variantData.options || []
+            const variantOptions = extractorsDataObj.customData.variantData.options
+            ?.map(o => o?.toLowerCase()) || []
             await page.evaluate(async(config, variantOptions) => {
-                const optionsToClick = Array.from(document.querySelectorAll(config.optionsSelector))
-                ?.filter(pdpOption => variantOptions?.find(variantOption => {
-                    if (config.matchWith == 'attribute'){
-                        const attributeNames = pdpOption?.getAttributeNames() || []
-                        const attributeValues = attributeNames?.map(att => pdpOption?.getAttribute(att)?.toLowerCase()) || []
-                        return attributeValues?.some(attVal => attVal == variantOption?.toLowerCase())
-                    } else if (config.matchWith == 'textContent'){
-                        const text = pdpOption?.textContent?.toLowerCase()?.trim()
-                        return text == variantOption?.toLowerCase()
-                    } else {
-                        return false
-                    }
-                }))
-                
+                const indexArr = []
+                getArray(config.elementsToMatch)
+                ?.filter((pdpOption, index) => {
+                    return variantOptions?.find(variantOption => {
+                        if (config.matchWith == 'attribute'){
+                            const attributeValues = getAttributeValues(pdpOption)
+                            const bool = attributeValues?.some(attVal => attVal == variantOption)
+                            if (bool){
+                                indexArr.push(index)
+                            }
+                            return bool
+                        } else if (config.matchWith == 'textContent'){
+                            const text = pdpOption?.textContent?.toLowerCase()?.trim()
+                            const bool = text == variantOption
+                            if (bool){
+                                indexArr.push(index)
+                            }
+                            return bool
+                        } else {
+                            return false
+                        }
+                    })
+                })
+                const optionsToClick = getArray(config.elementsToClick)
+                ?.filter((_, i) => indexArr?.includes(i))
+
                 for (let i = 0; i < optionsToClick?.length; i++){
                     optionsToClick[i]?.click()
                     await new Promise(r => setTimeout(r, config.timeout))
+                }
+
+                function getArray(selector){
+                    return Array.from(document.querySelectorAll(selector)) || []
+                }
+                function getAttributeValues(element){
+                    const attributeNames = element?.getAttributeNames() || []
+                    const attributeValues = attributeNames?.map(att => element?.getAttribute(att)?.toLowerCase()) || []
+                    return attributeValues
                 }
             }, config, variantOptions)
         }catch(e){}
